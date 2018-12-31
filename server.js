5fe212d409a7273d1de18f5b393e93f88a416b3e
Blake Netzeband <blake.netzeband@gmail.com>
2018-12-30 19:30:43 -0700

    added inert lib to serve static files. added basic html. added redirect for 404

--- server.js
+++ server.js
@@ -1,27 +1,56 @@
+"use strict";
+
 const Hapi = require("hapi");
+const Path = require("path");
+const Inert = require("inert");
 
-// init Hapi server
+// create hapi server object
 const server = Hapi.server({
-	host: "localhost",
-	port: "8080",
+	host: process.env.host || "localhost",
+	port: process.env.port || "8080",
+	routes: {
+		files: {
+			relativeTo: Path.join(__dirname, "public"),
+		},
+	},
 });
 
-// routes
-server.route({
-	method: "GET",
-	path: "/",
-	handler: (req, h) => "hello",
-});
+// declare init command
+const init = async () => {
+	await server.register(Inert);
 
-// declare start command and fire up the server
-const start = async () => {
-	try {
-		await server.start();
-	} catch (err) {
-		console.log("Error starting server:\n", err);
-		process.exit(1);
-	}
+	// define routes
+	server.route({
+		method: "GET",
+		path: "/{param*}",
+		handler: {
+			file: "index.html",
+		},
+	});
 
-	console.log(`Server running at ${server.info.uri}`);
+	// start hapi server object
+	await server.start();
+	console.log(`Haii! Server running at: ${server.info.uri}`);
 };
-start();
+
+// Redirect all routes to index
+server.ext("onPreResponse", (req, h) => {
+	const { response } = req;
+	// Boom is Hapi error library
+	if (response.isBoom && response.output.statusCode === 404) {
+		return h.file("index.html");
+	}
+	return h.continue;
+});
+
+// event handler for errors and warning
+process.on("unhandledRejection", err => {
+	console.log("Error:\n", `${err.message}\n`, err);
+	process.exit(1);
+});
+process.on("warning", warning => {
+	console.log(`Warning: ${warning.name}\n`, `${warning.message}\n`, warning.stack);
+});
+
+// FIRE IT UP!
+init();
