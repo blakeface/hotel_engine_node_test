ed75983cd48ef9e2c99cc63b66bb74c07fce120b
netzebandito <blake.netzeband@gmail.com>
2019-01-02 18:24:18 -0700

    added basic caching strategy for hapi

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# hotel_engine_node_test
+# Hotel Engine Node Test
+
 Technical Assessment for Hotel Engine
+
+## 🌋 Setup
+
+Typical NPM scripts:
+
+```
+$ npm install
+```
+
+```
+$ npm starts
+```
+
+## 🧰 Tools
+
+- Hapi (including Inert and catbox)
+- nodemon
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 "use strict";
 
 const Hapi = require("hapi");
-const Path = require("path");
 const Inert = require("inert");
+const Path = require("path");
+const request = require("request");
+const rp = require("request-promise-native");
+const catboxMongodb = require("catbox-mongodb");
 
 // create hapi server object
 const server = Hapi.server({
@@ -10,27 +13,61 @@ const server = Hapi.server({
 	port: process.env.port || "8080",
 	routes: {
 		files: {
-			relativeTo: Path.join(__dirname, "public"),
-		},
+			// keep all filepaths relative to public folder
+			relativeTo: Path.join(__dirname, "public")
+		}
 	},
+	cache: [
+		{
+			name: "mongoCache",
+			engine: catboxMongodb,
+			host: "127.0.0.1",
+			partition: "hotel_engine_node_test"
+		}
+	]
 });
 
 // declare init command
 const init = async () => {
 	await server.register(Inert);
 
+	// cache request object
+	const requestCache = server.cache({
+		cache: "mongoCache",
+		expiresIn: 10 * 1000,
+		segment: "gitHubRequest",
+		generateFunc: async id => {
+			// setup request options
+			const options = {
+				url:
+					"https://api.github.com/repos/vmg/redcarpet/issues?state=closed",
+				headers: {
+					"User-Agent": "hotel_engine_node_test", // name of repo
+					Accept: "application/vnd.github.v3+json"
+				}
+			};
+
+			// make request and return results
+			return await rp(options);
+		},
+		generateTimeout: 2000
+	});
+
 	// define routes
 	server.route({
 		method: "GET",
-		path: "/{param*}",
-		handler: {
-			file: "index.html",
-		},
+		path: "/{param*}", // currently don't pass url params, but defined for future use
+		handler: async function(req, h) {
+			// get cached gitHub request
+			const data = await requestCache.get();
+			console.log(data);
+
+			return h.file("index.html");
+		}
 	});
 
 	// start hapi server object
 	await server.start();
-	console.log(`Haii! Server running at: ${server.info.uri}`);
 };
 
 // Redirect all routes to index
@@ -49,7 +86,11 @@ process.on("unhandledRejection", err => {
 	process.exit(1);
 });
 process.on("warning", warning => {
-	console.log(`Warning: ${warning.name}\n`, `${warning.message}\n`, warning.stack);
+	console.log(
+		`Warning: ${warning.name}\n`,
+		`${warning.message}\n`,
+		warning.stack
+	);
 });
 
 // FIRE IT UP!
