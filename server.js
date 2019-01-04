6deb3ceddd765adb6a0600c056a4355975c46050
netzebandito <blake.netzeband@gmail.com>
2019-01-03 19:33:00 -0700

    finalized api call to github. started building view

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const Hapi = require("hapi");
 const Inert = require("inert");
 const Path = require("path");
 const request = require("request");
-const rp = require("request-promise-native");
+const rp = require("request-promise");
 const catboxMongodb = require("catbox-mongodb");
 
 // create hapi server object
@@ -35,23 +35,60 @@ const init = async () => {
 	await server.register(Inert);
 
 	// cache request object
-	const makeApiCall = async () => {
-		// make request and return results
-		return;
-		return await rp({
-			url: "https://api.github.com/repos/juliangarnier/anime/pulls",
+	const makeApiCall = () => {
+		const url = "https://api.github.com/repos/juliangarnier/anime/pulls";
+		let data = [];
+
+		const client_id = "0f51dae017fae2754f7e";
+		const client_secret = "324f6d0388360cf1c298a7e1703685d349ae59ba";
+		const authString = `?client_id=${client_id}&client_secret=${client_secret}`;
+
+		return rp({
+			url: url + authString,
 			headers: {
 				"User-Agent": "hotel_engine_node_test", // name of repo
 				Accept: "application/vnd.github.v3+json"
-			}
-		});
+			},
+			json: true
+		})
+			.then(results => {
+				// save data for future use
+				data = results.map(pull => ({
+					user: {
+						name: pull.user.login,
+						avatar: pull.user.avatar_url
+					},
+					id: pull.number
+				}));
+
+				return data;
+			})
+			.then(data => {
+				// get information about all pull requests
+				const promises = data.map(pull => {
+					return rp({
+						url: url + "/" + pull.id + authString,
+						headers: {
+							"User-Agent": "hotel_engine_node_test", // name of repo
+							Accept: "application/vnd.github.v3+json"
+						}
+					});
+				});
+
+				return Promise.all(promises);
+			})
+			.then(results => ({
+				users: data,
+				pulls: results
+			}))
+			.catch(err => console.log(err));
 	};
 
 	server.method("requestCache", makeApiCall, {
 		cache: {
 			cache: "mongoCache",
 			expiresIn: 10 * 1000,
-			generateTimeout: 2000,
+			generateTimeout: 10 * 1000,
 			getDecoratedValue: true
 		}
 	});
@@ -62,6 +99,11 @@ const init = async () => {
 		{
 			method: "GET",
 			path: "/",
+			handler: (req, h) => h.file("index.html")
+		},
+		{
+			method: "GET",
+			path: "/api/data",
 			handler: async function(req, h) {
 				// get cached gitHub request
 				const { value, cached } = await server.methods.requestCache();
@@ -72,8 +114,7 @@ const init = async () => {
 							: "freshly cached"
 					}`
 				);
-
-				return h.file("index.html");
+				return value;
 			}
 		},
 		// static files (css, js)
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import anime from "animejs";
+import axios from "axios";
 
 anime({
 	targets: ".loading-block",
@@ -14,3 +15,35 @@ anime({
 	easing: "easeInOutQuad",
 	duration: (el, i) => 1000 + i * 1000
 });
+
+// mock loading time of 2 seconds
+window.setTimeout(
+	() => axios.get("/api/data").then(results => thrashDom(results.data)),
+	2000
+);
+
+function thrashDom(results) {
+	const welcomeEl = document.querySelector(".welcome");
+	const appEl = document.querySelector(".app");
+
+	// update hidden classes
+	welcomeEl.classList.add("hidden");
+	appEl.classList.remove("hidden");
+
+	// add li
+	results.pulls.forEach((pull, i) => {
+		const li = createListItem(pull);
+		appEl.appendChild(li);
+	});
+}
+
+function createListItem(pull) {
+	const li = document.createElement("li");
+	const content = document.createTextNode(
+		"Pull Request: " + results.users.id
+	);
+	li.appendChild(content);
+	return li;
+}
+
+
